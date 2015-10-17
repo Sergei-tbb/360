a660df7c7aad18e6f11dd08fdff11a94bb2ca529
Olexandr <snigoviyolexandr@gmail.com>
2015-10-17 18:23:27 +0300

    create, update, delete operations for delevery streets

--- application/controllers/ajax/delivery/Delivery_streets.php
+++ application/controllers/ajax/delivery/Delivery_streets.php
@@ -13,6 +13,8 @@
  */
 class Delivery_streets extends MY_Controller {
 
+    var $returned_data;
+
     private function _get_table_name_from_class() {
         return strtolower(strval(__CLASS__));
     }
@@ -21,7 +23,63 @@ class Delivery_streets extends MY_Controller {
         parent::__construct(array("tbname" => $this->_get_table_name_from_class()));
     }
 
-    public function create_new_countrie() {
+    public function create_new_country() {
+        try {
+            $data = $this->input->post();
 
+            if (empty($data)) {
+                throw new Exception("Не правильные входные данные!");
+            }
+
+            if ($this->create($data) === false) {
+                throw new Exception("Не возможно добавить страну с текущими параметрами");
+            } else {
+                $this->returned_data = array("error" => false, "message"=>"Страна успешно добавлена");
+            }
+
+        } catch (Exception $exp) {
+            $this->returned_data = array("error" => true, "message" => $exp->getMessage());
+        }
+    }
+
+    public function update_current_country() {
+        try {
+            $data = $this->input->post();
+
+            $id = $data['id'];
+
+            unset($data['id']);
+
+            if (empty($id) || empty($data)) {
+                throw new Exception("Не верные параметры");
+            }
+
+            if ($this->update($id, $data) === false) {
+                throw new Exception("Не возможно обновить данные по стране текущими параметрами");
+            } else {
+                $this->returned_data = array("error" => false, "message" => "Данные по стране были успешно обновлены");
+            }
+
+        } catch (Exception $exp) {
+            $this->returned_data = array("error" => true, "message" => $exp->getMessage());
+        }
+    }
+
+    public function delete_current_country() {
+        try {
+            $data = $this->input->post();
+
+            if (empty($data)) {
+                throw new Exception("Не верные параметры");
+            }
+
+            if ($this->delete($data['id']) === false) {
+                throw new Exception("Не возможно удалить страну из базы");
+            } else {
+                $this->returned_data = array("error" => false, "message" => "Страна удалена из базы успешно");
+            }
+        } catch (Exception $ex) {
+            $this->returned_data = array("error" => true, "message" => $exp->getMessage());
+        }
     }
 }
